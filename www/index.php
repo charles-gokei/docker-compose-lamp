1d410b9f666d0e495889b9d8f55558e6c31f5575
Charles Sena <charlessena@gokeitecnologia.com.br>
2024-01-17 16:59:28 -0300

    fixup! WIP - Environment index page

--- www/index.php
+++ www/index.php
@@ -8,10 +8,20 @@
     <h2>Your local development environment</h2>
     <div>
       <ul>
-        <?php if(function_exists('apache_get_version')): ?>
-          <li><?= apache_get_version() ?></li>
-        <?php endif ?>
+        <li><?= apache_get_version() ?></li>
         <li>PHP <?= phpversion() ?></li>
+        <li>
+          <?php
+          $link = mysqli_connect('database','root', $_ENV['MYSQL_ROOT_PASSWORD']);
+          if(mysqli_connect_errno()) {
+            printf('MySQL connection failed: %s', mysqli_connect_error());
+          }
+          else {
+            printf('MySQL Server %s', mysqli_get_server_info($link));
+          }
+          mysqli_close($link);
+          ?>
+        </li>
       </ul>
     </div>
   </body>
