26f2450a06347947a2f1998123b77bb33647670a
Charles Sena <charlessena@gokeitecnologia.com.br>
2024-01-30 19:55:46 -0300

    fixup! Inclusão de arquivo para pagina web

--- www/index.php
+++ www/index.php
@@ -1,2 +1,24 @@
-<?php phpinfo();
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LAMP STACK</title>
+  </head>
+  <body>
+    <style>
+      @import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"
+    </style>
+    <section class="hero is-info">
+      <div class="hero-body">
+        <h1 class="title">LAMP STACK</h1>
+        <h2 class="subtitle">Your local development environment</h2>
+      </div>
+    </section>
+    <section class="section">
+      <div class="container">
+        <a href='/phpinfo.php'>phpinfo()</a>
+      </div>
+    </section>
+  </body>
+</html>
 
