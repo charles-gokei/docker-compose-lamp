b425869d66d943153cbf109313c57f25b0fab718
Charles Sena <charlessena@gokeitecnologia.com.br>
2024-02-23 16:47:19 -0300

    fixup! fixup! fixup! fixup! fixup! fixup! WIP - Reverse enginering work

--- www/index.php
+++ www/index.php
@@ -3,14 +3,18 @@
 <html>
   <head>
     <title>LAMP STACK</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
   </head>
   <body>
-    <header>
-     <h1>LAMP STACK</h1>
-     <h2>Your local development environment</h2>
+    <header class="hero is-info has-text-centered is-medium">
+     <div class="hero-body">
+       <h1 class="title">LAMP STACK</h1>
+       <h2 class="subtitle">Your local development environment</h2>
+     </div>
     </header>
-    <section>
-      <header>Environment</header>
+    <section class="section">
+      <header class='title'><h3>Environment</h3></header>
       <ul>
         <li><?= apache_get_version() ?></li>
         <li>PHP <?= phpversion() ?></li>
@@ -24,8 +28,8 @@
         ?></li>
       </ul>
     </section>
-    <section>
-      <header>Quick links</header>
+    <section class="section">
+      <header class='title'><h3>Quick links</h3></header>
       <ul>
         <li><a href="/phpinfo.php">phpinfo()</a></li>
         <li><a href="http://localhost:8081">phpMyAdmin</a></li>
